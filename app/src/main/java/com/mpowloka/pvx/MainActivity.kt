de97ca9ab3b897f5145a4524eca3199f31ec00e0
mpowloka <michalpowloka@gmail.com>
2019-05-11 16:31:17 +0200

    Add basic Action Bar

--- app/src/main/java/com/mpowloka/pvx/MainActivity.kt
+++ app/src/main/java/com/mpowloka/pvx/MainActivity.kt
@@ -2,11 +2,22 @@ package com.mpowloka.pvx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+        setSupportActionBar(main_toolbar)
+        supportActionBar?.setTitle(R.string.pvx)
+        supportActionBar?.setSubtitle(R.string.search)
+    }
+
 }
